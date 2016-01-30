4ec48e02f29dc8d43fb589f99aed1d18419fe595
Dineth Hettiarachchi <dineth@sachintha.com>
2016-01-30 22:24:28 +0000

    initial commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+
+<html>
+  <head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, minimum-scale=1.0, initial-scale=1, user-scalable=yes">
+
+    <title>PayPal Form</title>
+
+    <script src="bower_components/webcomponentsjs/webcomponents-lite.js"></script>
+
+
+    <link rel="import" href="paypal-form.html">
+
+  </head>
+
+    <body>
+        
+        <paypal-form></paypal-form>
+
+    
+        
+    </body>
+
+
+</html>
